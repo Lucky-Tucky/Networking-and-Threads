c5a781e561a70e791f211dd1b852dabdcb1796ae
Lakshay Chauhan <lakshaychauhan129@gmail.com>
2026-09-11 13:22:03 +0530

    GET method request ready server

--- src/main/java/org/lucky/HttpServer/HttpMethod.java
+++ src/main/java/org/lucky/HttpServer/HttpMethod.java
@@ -1,47 +1,66 @@
 package org.lucky.HttpServer;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.nio.channels.Channels;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 public class HttpMethod implements Runnable{
 
-    private final String request;
-    private Map<String,String > headers = new HashMap<>();
-    private String body = null;
-    private String method;
+    private final Map<String,String > headers = new HashMap<>();
+    private final String body = null;
+    private String methodType;
+    private final SocketChannel socketChannel;
+    private String HttpRequest;
+    private final StringBuffer log;
 
-    public HttpMethod(String request){
-        this.request = request;
+    public HttpMethod(SocketChannel socketChannel){
+        log = new StringBuffer(LocalDateTime.now().toString()).append("\n");
+        this.socketChannel = socketChannel;
     }
 
     private void cleanUp(){
         try {
-            String[] request_parts = request.split("\n");
-            String[] request_line =  request_parts[0].split(" ");
-            this.method = request_line[0];
+            String[] request_parts = HttpRequest.split("\n");
+            this.methodType = methodType(request_parts[0]);
+            if(this.methodType==null){
+                throw new Exception("Method Not Found");
+            }
+            log.append(request_parts[0]).append("\n");
             int i =1;
-            while(request_parts[i].contains(":")){
-                String[] header = request_parts[i].split(":");
-                this.headers.put(header[0],header[1]);
+            while(i<request_parts.length && request_parts[i].contains(":")){
+                String[] header = request_parts[i].split(":",2);
+                this.headers.put(header[0],header[1].trim());
                 i++;
             }
 
         }catch (Exception e){
+            log.append("Method Not Found").append("\n");
             e.printStackTrace();
-        }
-        finally {
-            HttpMethod.logger(LocalDateTime.now() + " "+this.method!=null ? this.method : "Error" );
         }
     }
 
-    private static synchronized void logger(String text){
+    private String methodType(String request_line){
+        System.out.println(request_line);
+        if(request_line.contains("GET")){
+            return "GET";
+        } else if (request_line.contains("PUT")) {
+            return "PUT";
+        }else if (request_line.contains("DELETE")){
+            return "DELETE";
+        }else if(request_line.contains("POST")){
+            return "POST";
+        }else{
+            return null;
+        }
+    }
+
+    private static synchronized void logger(String logs){
         try(BufferedWriter writer = new BufferedWriter(new FileWriter("logs.txt",true))) {
-            writer.append(text);
+            writer.append(logs);
             writer.newLine();
         }catch (IOException e){
             e.printStackTrace();
@@ -51,6 +70,26 @@ public class HttpMethod implements Runnable{
 
     @Override
     public void run() {
-//        cleanUp();
+        try{
+            Reader reader = Channels.newReader(socketChannel, StandardCharsets.UTF_8);
+            log.append(socketChannel.getRemoteAddress().toString()).append("\n");
+            BufferedReader bufferedReader  = new BufferedReader(reader);
+
+            StringBuilder request = new StringBuilder();
+            String line = "";
+            while((line=bufferedReader.readLine())!=null && !line.isEmpty()) {
+                request.append(line).append("\n");
+            }
+            this.HttpRequest = request.toString();
+            if(!this.HttpRequest.isEmpty()){
+                this.cleanUp();
+            }
+
+        }catch (IOException e){
+            log.append("Request not Fulfilled");
+            e.printStackTrace();
+        }finally {
+            HttpMethod.logger(log.toString());
+        }
     }
 }
--- src/main/java/org/lucky/HttpServer/HttpServer.java
+++ src/main/java/org/lucky/HttpServer/HttpServer.java
@@ -1,41 +1,27 @@
 package org.lucky.HttpServer;
 
-import java.io.BufferedReader;
-import java.io.Reader;
 import java.net.InetSocketAddress;
-import java.nio.channels.Channels;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
-import java.nio.charset.StandardCharsets;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class HttpServer {
 
     static void main() {
 
         InetSocketAddress address = new InetSocketAddress(8080);
-
+        ExecutorService threadPool = Executors.newCachedThreadPool();
         try(ServerSocketChannel channel = ServerSocketChannel.open()){
             channel.bind(address);
-
             while(channel.isOpen()){
                 SocketChannel communicationChannel = channel.accept();
-                Reader reader = Channels.newReader(communicationChannel, StandardCharsets.UTF_8);
-                BufferedReader bufferedReader = new BufferedReader(reader);
-
-                StringBuffer request = new StringBuffer();
-                String line = "";
-                while((line=bufferedReader.readLine())!=null){
-                    request.append(line+"\n");
-                }
-                System.out.println(request.toString());
-//                new Thread(new HttpMethod(request.toString())).start();
-                bufferedReader.close();
+                threadPool.execute(new HttpMethod(communicationChannel));
             }
-
-
         } catch (Exception e) {
             e.printStackTrace();
+        }finally {
+            threadPool.shutdown();
         }
-
     }
 }
